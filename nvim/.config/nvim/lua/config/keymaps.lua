890e25fa52210b2e24fa775b425fa459508e23b5
Gideon <gideon.merrick.dev@gmail.com>
2026-02-25 00:45:01 +0100

    updated mappings and added trouble

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<c-p>", "<cmd>Telescope find_files<cr>", { desc = "Find files" });
 vim.keymap.set("n", "<c-e>", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle file explorer" });
+vim.keymap.set("n", "<c-x>", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Toggle diagnostics panel" })
